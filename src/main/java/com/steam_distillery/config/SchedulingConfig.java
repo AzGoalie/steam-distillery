8dd1f273581bddf792b232736e2afb76fbd00554
Travis McMahon <azgoalie@gmail.com>
2022-06-23 22:51:44 -0700

    Enabled scheduling via property

--- src/main/java/com/steam_distillery/config/SchedulingConfig.java
+++ src/main/java/com/steam_distillery/config/SchedulingConfig.java
@@ -1,11 +1,11 @@
 package com.steam_distillery.config;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@Profile("!dev")
 @Configuration
 @EnableScheduling
+@ConditionalOnProperty(value = "scheduling.enabled", havingValue = "true")
 public class SchedulingConfig {
 }
